891b369840f21e230de30c92c121941fc9320d26
kevincallaghan <kevincallaghan5@yahoo.com>
2023-06-04 12:13:13 -0500

    Fixed Service Worker Similar To Class Example

--- client/src-sw.js
+++ client/src-sw.js
@@ -4,7 +4,6 @@ const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
-const { Workbox } = require('workbox-window');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -28,13 +27,18 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
+
 registerRoute(
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-  new offlineFallback({
+  
+  new CacheFirst({
     cacheName: 'asset-cache',
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60,
       }),
     ],
   })
